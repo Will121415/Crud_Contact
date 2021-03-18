c0ee711f9d17c967286b92eb9a6fb8e9499a8155
MoraWill <wifermen@hotmail.com>
2021-03-18 13:48:50 -0500

    fitst commit

---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:crud_contact/modify.dart';
+import 'package:crud_contact/register.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const String myTitle = 'Contact By Will';
+    return MaterialApp(
+      title: myTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: myTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Client> clients = [
+    Client(name: 'Will', surname: 'Mora', phone: '302 454 25 93'),
+    Client(name: 'Sam', surname: 'Perez', phone: '322 412 34 68'),
+    Client(name: 'Marlon', surname: 'Gutierrez', phone: '301 441 36 98'),
+    Client(name: 'Jhon', surname: 'Segura', phone: '310 789 90 62')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+          itemCount: clients.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              onTap: () {
+                print(index);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => Modify(clients[index]))).then((value) {
+                  if (value != null) {
+                    setState(() {
+                      clients.removeAt(index);
+                      clients.insert(index, value);
+                      _messageResponde(
+                          context, value.name + ' a sido actualizado...!');
+                    });
+                  }
+                });
+              },
+              onLongPress: () {
+                setState(() {
+                  _removeClient(context, clients[index]);
+                });
+              },
+              title: Text(clients[index].name + ' ' + clients[index].surname),
+              subtitle: Text(clients[index].phone),
+              leading: CircleAvatar(
+                child: Text(clients[index].name.substring(0, 1)),
+              ),
+              trailing: Icon(Icons.arrow_circle_up),
+            );
+          }),
+
+      //Paso # 1
+      //Este es el Boton Adicionar
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) =>
+                  Register(widget.title), //Llamar la Vista TextoEjercicio
+            ),
+          ).then((result) {
+            if (result != null) {
+              setState(() {
+                clients.add(result);
+                _messageResponde(context, result.name + ' a sido guardado...!');
+              });
+            }
+          });
+        },
+        tooltip: 'Adicionar Client',
+        child: Icon(Icons.add),
+      ),
+
+      //
+    );
+  }
+
+  _messageResponde(BuildContext context, String name) {
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              title: Text('Mensaje Informativo...!'),
+              content: Text('El contacto $name'),
+            ));
+  }
+
+  _removeClient(context, Client client) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Elimnar Cliente...!'),
+        content: Text('Esta Seguro de Eliminar' + client.name + '?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              this.clients.remove(client);
+              Navigator.pop(context);
+            },
+            child: Text(
+              'Eliminar',
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              'Cancelar',
+              style: TextStyle(color: Colors.blue),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Client {
+  var name;
+  var surname;
+  var phone;
+
+  Client({this.name, this.surname, this.phone});
+}
---
+++ lib/modify.dart
@@ -0,0 +1,107 @@
+import 'package:crud_contact/main.dart';
+import 'package:flutter/material.dart';
+
+class Modify extends StatefulWidget {
+  final Client client;
+  Modify(this.client, {Key key}) : super(key: key);
+
+  @override
+  _ModifyState createState() => _ModifyState();
+}
+
+class _ModifyState extends State<Modify> {
+  String name, surname, phone;
+
+  TextEditingController controllerName;
+  TextEditingController controllerSurname;
+  TextEditingController controllerPhone;
+
+  @override
+  void initState() {
+    Client c = widget.client;
+    controllerName = TextEditingController(text: c.name);
+    controllerSurname = TextEditingController(text: c.surname);
+    controllerPhone = TextEditingController(text: c.phone);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Modificar Contacto'),
+        ),
+        body: ListView(
+          children: [
+            TextBox(
+              controlador: controllerName,
+              label: 'Nombre',
+            ),
+            TextBox(
+              controlador: controllerSurname,
+              label: 'Apellido',
+            ),
+            TextBox(
+              controlador: controllerPhone,
+              label: 'Telefono',
+            ),
+
+            // Boton
+            ElevatedButton(
+              onPressed: () {
+                name = controllerName.text;
+                surname = controllerSurname.text;
+                phone = controllerPhone.text;
+
+                if (name.isNotEmpty && surname.isNotEmpty) {
+                  Navigator.pop(context,
+                      Client(name: name, surname: surname, phone: phone));
+                }
+              },
+              child: Text('Actualizar'),
+            ),
+
+            //
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextBox extends StatelessWidget {
+  final label;
+  const TextBox({
+    this.label,
+    Key key,
+    @required this.controlador,
+  }) : super(key: key);
+
+  final TextEditingController controlador;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: TextField(
+          controller: controlador,
+          decoration: InputDecoration(
+              filled: true,
+              labelText: this.label,
+              // suffix: Icon(Icons.access_alarm),
+              suffix: GestureDetector(
+                child: Icon(Icons.close),
+                onTap: () {
+                  controlador.clear();
+                },
+              )
+              //probar suffix
+              ),
+          onSubmitted: (String nombres) {
+            // cajatexto(context, nombres);
+          }),
+    );
+  }
+}
